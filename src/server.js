90db7dfe6073f48a0539e0c8ad002344ffbd3098
fuad-dev-portfolio <abdullahalfuad.dev@gmail.com>
2026-01-20 22:16:54 +0600

    chore: Basic server setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config({path:"./src/.env"});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    res.json({message: "Hellow , I am a professional devops enggineer"})
+});
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`server is listen on port: ${process.env.PORT}`)
+});
